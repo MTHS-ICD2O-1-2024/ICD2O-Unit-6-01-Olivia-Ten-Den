27f1f9c44a159f411eb3fd8d9a7b9641900d2a55
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-13 17:09:34 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: April 24, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function checkNumber() {
+  // input
+  const ageNumber = parseInt(document.getElementById("age-number").value)
+
+  // process
+  if (ageNumber >= 17) {
+    //output
+    document.getElementById("result").innerHTML =
+      "<p>You can see an R movie alone.</p>"
+  }
+  // process
+  else if (ageNumber >= 13) {
+    //output
+    document.getElementById("result").innerHTML =
+      "<p>You can see a PG-13 movie alone.</p>"
+  }
+    // process
+  else if (ageNumber >= 5) {
+    //output
+    document.getElementById("result").innerHTML =
+      "<p>You can see a G or PG movie alone.</p>"
+  }
+    // process
+  else  {
+    //output
+    document.getElementById("result").innerHTML =
+      "<p>I think you may be to young to watch a movie by yourself. :(</p>"
+  }
+}
