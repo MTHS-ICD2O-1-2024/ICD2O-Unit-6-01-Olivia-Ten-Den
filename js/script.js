f0f40a3ce242aa1144fa59f1d003508cb0d3ba3e
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-19 21:46:52 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Olivia Ten Den
-// Created on: May 13, 2025
+// Created on: May 6, 2025
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,37 +10,17 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICD2O-Unit-6-01-Olivia-Ten-Den/sw.js", {
-    scope: "/ICD2O-Unit-6-01-Olivia-Ten-Den/",
+  navigator.serviceWorker.register("/ICD2O-Unit-6-01b-Olivia-Ten-Den/sw.js", {
+    scope: "/ICD2O-Unit-6-01b-Olivia-Ten-Den/",
   })
 }
 
-function checkNumber() {
-  // input
-  const ageNumber = parseInt(document.getElementById("age-number").value)
+function calculate() {
+  let answer = 0
+  let integer = parseInt(document.getElementById("integer").value)
 
-  // process
-  if (ageNumber >= 17) {
-    //output
-    document.getElementById("result").innerHTML =
-      "<p>You can see an R movie alone.</p>"
+  for (let counter = 1; counter <= integer; counter++) {
+    answer = answer + counter
   }
-  // process
-  else if (ageNumber >= 13) {
-    //output
-    document.getElementById("result").innerHTML =
-      "<p>You can see a PG-13 movie alone.</p>"
-  }
-  // process
-  else if (ageNumber >= 5) {
-    //output
-    document.getElementById("result").innerHTML =
-      "<p>You can see a G or PG movie alone.</p>"
-  }
-  // process
-  else {
-    //output
-    document.getElementById("result").innerHTML =
-      "<p>I think you may be to young to watch a movie by yourself. :(</p>"
-  }
+  document.getElementById('result').innerHTML = "<p>The product is " + answer + ".</p>"
 }
